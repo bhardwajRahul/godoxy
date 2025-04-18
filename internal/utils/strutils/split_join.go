b72c08b3b9c040a9b7a488b3f1cc506e036f3109
yusing <yusing@6uo.me>
2025-04-19 07:19:21 +0800

    chore: update LICENSE and add copyright information
    - Update copyright year from 2024 to 2025 and change the name to [yusing]
    - Add licensing information for copied files from Go Authors and ByteDance Inc.
    - Include notes on modifications made to the copied files for project adaptation

--- internal/utils/strutils/split_join.go
+++ internal/utils/strutils/split_join.go
@@ -1,3 +1,8 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// This is a copy of strings.Split and strings.Join with modifications to adapt to this project.
 package strutils
 
 import (
